5554b55d0a7bcdead04f3ee6dae2116223accc3a
EvyPurple1 <evysaba@gmail.com>
2017-08-02 11:51:29 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'xo$2+7&_+-)%%#s!ls(t8_1cei31iy_&htr(bj=836wl8nf1wc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'evy_violette.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'evyviolette.pythonanywhere.com']
 
 
 # Application definition
